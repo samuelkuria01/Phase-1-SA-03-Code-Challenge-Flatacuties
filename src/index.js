75e9caf0b78578fa0a3b2a942d5fa702467448b6
sammy <118251741+samuelkuria01@users.noreply.github.com>
2022-12-12 20:02:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,70 @@
 document.addEventListener("DOMContentLoaded", () =>{
-
-  document.getElementById("create-task-form").addEventListener("submit",(event) =>{
-    event.preventDefault();
-    const description= document.querySelector("#new-task-description").value;
-        toDoList(description);
-
+function () ;
 })
+
+
+  const url = "http://localhost:3000/characters";
+  const characterBar = document.getElementById("character-bar");
+  const characterName = document.getElementById("name");
+  const characterImage = document.getElementById("image");
+  const characterVoteCount = document.getElementById("vote-count");
+  const characterVoteForm = document.getElementById("votes-form");
+
+
+  function fetchData() {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        renderCharacters(data);
+      });
+    
+    
+    
+     function renderCharacters(data) {
+    data.forEach((data) => {
+      const nameSpan = document.createElement("span");
+      nameSpan.innerText = data.name;
+      characterBar.appendChild(nameSpan);
+      nameSpan.addEventListener("click", () => {
+        characterName.textContent = data.name;
+        characterImage.setAttribute("src", data.image);
+        characterVoteCount.textContent = data.votes;
+      });
+    });
+       
+       
+       
+  }
+    characterVoteForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newVotes = parseInt(event.target.votes.value);
+    const characterVoteCount = document.getElementById("vote-count");
+    let current = parseInt(characterVoteCount.textContent);
+    let votecount = (current += newVotes);
+    characterVoteCount.innerText = votecount;
+
+    //Updating the database
+    let updateVotes = {
+      votes: votecount,
+    };
+      
+      
+      
+      
+        fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json; charset=UTF-8",
+        Authorization: "",
+      },
+      method: "PATCH",
+      body: JSON.stringify({
+        votes: votecount,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  });
+      
+  
+    
